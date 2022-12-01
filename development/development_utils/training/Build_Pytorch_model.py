e692a370f24638d0c1537c50d88ad127f68444b3
StyrbjornKall <skall@chalmers.se>
2022-12-01 16:40:18 +0100

    Fixed bugs

--- development/development_utils/training/Build_Pytorch_model.py
+++ development/development_utils/training/Build_Pytorch_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from typing import List
+import numpy as np
 
 
 def GPUinfo(device):
--- development/development_utils/training/PerformanceCalculations.py
+++ development/development_utils/training/PerformanceCalculations.py
@@ -3,24 +3,24 @@ import numpy as np
 from typing import List, TypeVar
 
 def CalculateWeightedAverage(df):
-    medians = df.groupby(['COMBINED_CAS','COMBINED_Duration_Value',
-       'COMBINED_effect','COMBINED_endpoint', 'SMILES','SMILES_Canonical_RDKit',
+    medians = df.groupby(['CAS','Duration_Value',
+       'effect','endpoint', 'SMILES','SMILES_Canonical_RDKit',
        'cmpdname'], as_index=False, dropna=False).median()
-    counts = df.groupby(['COMBINED_CAS','COMBINED_Duration_Value',
-       'COMBINED_effect','COMBINED_endpoint', 'SMILES','SMILES_Canonical_RDKit',
+    counts = df.groupby(['CAS','Duration_Value',
+       'effect','endpoint', 'SMILES','SMILES_Canonical_RDKit',
        'cmpdname'], as_index=False, dropna=False).count()
     counts.rename(columns={'labels': 'counts'}, inplace=True)
 
     medians['counts'] = counts['counts']
     for col in medians.columns:
-        if col not in ['COMBINED_CAS','COMBINED_effect','COMBINED_endpoint', 'SMILES','SMILES_Canonical_RDKit','cmpdname','counts']:
+        if col not in ['CAS','effect','endpoint', 'SMILES','SMILES_Canonical_RDKit','cmpdname','counts']:
             medians[[str(col)]] = medians[[str(col)]]*medians[['counts']].to_numpy()
 
-    mean = medians.groupby(['COMBINED_CAS','COMBINED_endpoint','SMILES','SMILES_Canonical_RDKit','cmpdname'], as_index=False, dropna=False).sum(min_count=1)
+    mean = medians.groupby(['CAS','endpoint','SMILES','SMILES_Canonical_RDKit','cmpdname'], as_index=False, dropna=False).sum(min_count=1)
 
     for col in mean.columns:
         try:
-            if col not in ['COMBINED_CAS','COMBINED_endpoint','SMILES','SMILES_Canonical_RDKit','cmpdname','counts']:
+            if col not in ['CAS','endpoint','SMILES','SMILES_Canonical_RDKit','cmpdname','counts']:
                 mean[str(col)] = mean[[str(col)]]/mean[['counts']].to_numpy()
         except:
             pass
